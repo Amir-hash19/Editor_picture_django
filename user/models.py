d57e839a1d1c872894447e1f2c1033b3a8198bf5
amirhoseinheydari <amirhosein.hydri1381@gmail.com>
2025-04-08 21:10:26 +0330

    set templates and static files

--- user/models.py
+++ user/models.py
@@ -1,16 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
-class Account(models.Model):
+
+class AccountManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, email, password, **extra_fields)
+
+
+
+
+
+
+class Account(AbstractBaseUser):
     full_name = models.CharField(max_length=150, unique=True)
     username = models.CharField(max_length=120, unique=True)
     email = models.EmailField(unique=True)
-    phone_number = models.PositiveIntegerField(null=True, blank=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)  
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)  
+    is_superuser = models.BooleanField(default=False)
     date_created = models.DateTimeField(auto_now_add=True)
 
+    
+    USERNAME_FIELD = 'email'  
+    REQUIRED_FIELDS = ['username', 'full_name']  
+
+
+
+    objects = AccountManager()  
 
     def __str__(self):
-        return f"{self.full_name}"
+        return self.full_name
     
 
+    def has_perm(self, perm, obj=None):
+        return True
+
+
+    def has_module_perms(self, app_label):
+        return True
+    
+    @property
+    def is_authenticated(self):
+        return True
