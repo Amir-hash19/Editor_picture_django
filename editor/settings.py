b69725b4761db69dbd4fb69611819842d1ed12dd
amirhoseinheydari <amirhosein.hydri1381@gmail.com>
2025-04-08 12:36:57 +0330

    set postgreSQL

--- editor/settings.py
+++ editor/settings.py
@@ -73,13 +73,15 @@ WSGI_APPLICATION = 'editor.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "editorpic",
+        "USER": "admin",
+        "PASSWORD": "amir112233",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
