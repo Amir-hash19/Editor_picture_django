4dd2cdf66276abdfad81482844b2b8b08e3000e8
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-04-08 13:14:14 +0330

    set models and register in admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Account
 
-# Register your models here.
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "email")
+    search_fields = ("is_active","phone_number")
+    list_filter = ("date_created", )
+    
+
--- user/models.py
+++ user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    full_name = models.CharField(max_length=150, unique=True)
+    username = models.CharField(max_length=120, unique=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.PositiveIntegerField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.full_name}"
+    
+
